13eb7f90b93e93ccf71b3eec9670af73182f07d6
AllanBarros <allanbmalves@gmail.com>
2021-07-19 18:53:07 -0300

    Ajustes de encode e bloqueio na tela.

--- tweet-moderation-front/src/pages/HashtagSelection.js
+++ tweet-moderation-front/src/pages/HashtagSelection.js
@@ -8,9 +8,18 @@ function HashTagSelection() {
 
     const [hashtag, setHash] = useState('')
     const [lista, setLista] = useState([])
+    const [valido, setValido] = useState(false)
 
     const handle_hashtag = (e) =>{
-        return setHash(e.target.value);
+        let pattern = "[a-zA-Z]*[1-9]*" ;
+        let valor = e.target.value
+        if (valor.match(pattern) != "") {
+            setValido(true)
+            return setHash(e.target.value);
+        }else{
+            setHash(e.target.value);
+            return setValido(false);
+        }
     }
 
     const buscar_hashtag = () =>{
@@ -31,8 +40,9 @@ function HashTagSelection() {
             <Grid container spacing={2} className="espacamento">
                 <Grid item xs={1}></Grid>
                 <Grid item xs={10}>
-                    <TextField className="ocupar" variant="outlined" required id="standard-required" label="Digite a hashtag para busca"  value={hashtag} onChange={(e) =>{handle_hashtag(e)}}/>
-                    <Button className="margem" variant='contained' color='primary' fullWidth={true} onClick={()=>{buscar_hashtag()}}>Buscar</Button>
+
+                    <TextField error={!valido} helperText={"Digite apenas a palavra sem #"} className="ocupar" variant="outlined" required id="standard-required" label="Digite a hashtag para busca"  value={hashtag} onChange={(e) =>{handle_hashtag(e)}}/>
+                    <Button disabled={!valido} className="margem" variant='contained' color='primary' fullWidth={true} onClick={()=>{buscar_hashtag()}}>Buscar</Button>
                 </Grid>
                 <Grid item xs={1}></Grid>
             </Grid>
